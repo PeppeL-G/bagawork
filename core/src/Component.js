0497ff303eb225035a0efbb96f1e71b7ba7c589a
Peppe L-G <peter.larsson.green@gmail.com>
2023-09-29 08:38:41 +0200

    Core: Added the configuration method cornerRadius().

--- core/src/Component.js
+++ core/src/Component.js
@@ -4,6 +4,7 @@ export class Component{
 		
 		this.backgroundColor = props.backgroundColor?.[0] ?? ""
 		this.size = props.size?.[0] ?? null
+		this.cornerRadius = props.cornerRadius?.[0] ?? 0
 		
 	}
 	
