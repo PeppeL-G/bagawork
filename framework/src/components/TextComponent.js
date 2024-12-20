35c6a583c57972792117032a8e1a4d2dfb72601e
Peppe L-G <peter.larsson.green@gmail.com>
2024-12-20 12:27:28 +0100

    Fixed a bug where the Text component didn't show all of its text if it overflowed (horizontally).

--- framework/src/components/TextComponent.js
+++ framework/src/components/TextComponent.js
@@ -104,6 +104,7 @@ export class TextComponent extends Component{
 		textElement.style.overflow = 'auto'
 		
 		const contentElement = document.createElement(`span`)
+		contentElement.style.maxWidth = `100%`
 		contentElement.style.maxHeight = `100%`
 		
 		// Fix HTML.
